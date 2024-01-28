1ec94de59c9a581445d6b1ec692970f09cd3f001
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-01-28 23:46:44 +0600

    example of theme data and app color

--- lib/controllers/utils/app_colors.dart
+++ lib/controllers/utils/app_colors.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
-  static Color backgroundColor = Color(0XFFFFFFFF);
+  static Color backgroundColor = const Color(0XFFFFFFFF);
+  static Color darkBackground = const Color(0XFF142850);
+  static Color appbarColor = const Color(0XFFE3F6F5);
+  static Color cardColor = const Color(0XFFBAE8E8);
+  static Color buttonColor = const Color(0XFF272343);
+  static Color primaryColor = const Color(0XFFE8F9FD);
+  static Color secondaryColor = const Color(0XFF0AA1DD);
+  static Color fontColor = const Color(0XFF0C134F);
+  static Color shadowColor = const Color(0XFF96EFFF);
 }
--- lib/controllers/utils/app_theme.dart
+++ lib/controllers/utils/app_theme.dart
@@ -1,7 +1,92 @@
+import 'package:besic_structure/controllers/utils/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   static ThemeData getAppTheme() {
-    return ThemeData();
+    return ThemeData(
+      primaryColorLight: AppColor.backgroundColor,
+      primaryColorDark: AppColor.darkBackground,
+      appBarTheme: AppBarTheme(
+        elevation: 1.5,
+        backgroundColor: AppColor.appbarColor,
+        shadowColor: AppColor.shadowColor,
+        titleTextStyle: GoogleFonts.cormorantGaramond(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          color: AppColor.fontColor,
+        ),
+      ),
+      iconTheme: IconThemeData(
+        color: AppColor.fontColor,
+      ),
+      colorScheme: ColorScheme(
+        background: AppColor.backgroundColor,
+        onBackground: AppColor.backgroundColor,
+        brightness: Brightness.light,
+        primary: AppColor.primaryColor,
+        onPrimary: AppColor.primaryColor,
+        secondary: AppColor.appbarColor,
+        onSecondary: AppColor.appbarColor,
+        error: Colors.red,
+        onError: Colors.red,
+        surface: AppColor.backgroundColor,
+        onSurface: AppColor.backgroundColor,
+      ),
+      scaffoldBackgroundColor: AppColor.backgroundColor,
+      cardTheme: CardTheme(
+        color: AppColor.cardColor,
+        shadowColor: AppColor.shadowColor,
+      ),
+      // text theme data=============================
+      textTheme: TextTheme(
+        displayLarge: GoogleFonts.cormorantGaramond(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: AppColor.fontColor,
+        ),
+        displayMedium: GoogleFonts.cormorantGaramond(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          color: AppColor.fontColor,
+        ),
+        displaySmall: GoogleFonts.cormorantGaramond(
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+          color: AppColor.fontColor,
+        ),
+        titleLarge: GoogleFonts.cormorantGaramond(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          color: AppColor.fontColor,
+        ),
+        titleMedium: GoogleFonts.cormorantGaramond(
+          fontSize: 14,
+          fontWeight: FontWeight.bold,
+          color: AppColor.fontColor,
+        ),
+        titleSmall: GoogleFonts.cormorantGaramond(
+          fontSize: 12,
+          fontWeight: FontWeight.bold,
+          color: AppColor.fontColor,
+        ),
+        bodyLarge: GoogleFonts.cormorantGaramond(
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+          color: AppColor.fontColor,
+        ),
+        bodyMedium: GoogleFonts.cormorantGaramond(
+          fontSize: 14,
+          fontWeight: FontWeight.bold,
+          color: AppColor.fontColor,
+        ),
+        bodySmall: GoogleFonts.cormorantGaramond(
+          fontSize: 12,
+          fontWeight: FontWeight.bold,
+          color: AppColor.fontColor,
+        ),
+      ),
+      //   text theme data ends ================
+    );
   }
 }
